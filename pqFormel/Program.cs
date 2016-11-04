0f74e2f0aa487f89d07dba6c019971a29cb7596b
Johannes Becker <j.becker@faz.de>
2016-11-04 15:46:55 +0100

    Add project files.

---
+++ pqFormel/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pqFormel
+{
+    class Program
+    {
+        double a, p, q, d, w;
+        static void Main(string[] args)
+        {
+            Program pqFormel = new Program();
+            pqFormel.eingabeWerte();
+            pqFormel.teileDurchA();
+            pqFormel.berechneDeterminante();
+            pqFormel.berechneWurzel();
+            pqFormel.berechneNullstellen();
+            Console.ReadKey();
+        }
+
+        public void teileDurchA()
+        {
+            if (a > 1)
+            {
+                p = p / a;
+                q = q / a;
+            }
+        }
+
+        public void berechneDeterminante()
+        {
+            d = Math.Pow((p / 2.0), 2) - q;
+        }
+
+        public void berechneWurzel()
+        {
+            if (d >= 0)
+            {
+                w = Math.Sqrt(d);
+            }
+            else
+                Console.WriteLine("wurzel ist negativ!");
+        }
+
+        public void berechneNullstellen()
+        {
+            if (w == 0)
+            {
+                Console.WriteLine("Einzige Nullstelle ist: " + (-(p / 2)));
+            }
+            else
+            {
+                Console.WriteLine("Erste Nullstelle ist: " + (-(p / 2) + w));
+                Console.WriteLine("Zweite Nullstelle ist: " + (-(p / 2) - w));
+            }
+        }
+
+        public void eingabeWerte()
+        {
+            Console.Write("a = ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("p = ");
+            p = Convert.ToDouble(Console.ReadLine());
+            Console.Write("q = ");
+            q = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
---
+++ pqFormel/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("pqFormel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("pqFormel")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c61b46c4-513f-45d5-a020-3f3f9bdf14a1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
